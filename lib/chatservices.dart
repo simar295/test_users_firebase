ac7c52aeb5c4e5c0fe0a2ab94bf4ad451a83d8f3
Simarpreet Singh <110830402+simar295@users.noreply.github.com>
2023-08-14 00:55:35 +0530

    Update chatservices.dart

--- lib/chatservices.dart
+++ lib/chatservices.dart
@@ -4,8 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'messageclass.dart';
+
 
 class chatservice {
   var getusername = "";
